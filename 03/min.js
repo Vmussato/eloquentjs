b71a4966c50909ba99ca146d271776721647eaf9
Vinicius Mussato <vinicius.mussato@gmail.com>
2017-11-02 16:40:55 -0200

    min.js

---
+++ 03/min.js
@@ -0,0 +1,12 @@
+function min(number1 , number2) {
+	if (number1 === number2) {
+		return "Same value numbers";
+	} else if (number1 > number2) {
+		return number1;
+	} else {
+		return number2;
+	}
+}
+
+console.log(min(0, 10));
+console.log(min(0, -10));
